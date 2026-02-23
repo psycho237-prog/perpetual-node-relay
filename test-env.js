800aed7dc7862d40ccda2db85319c37f4cf861d3
Psycho <psycho@example.com>
2026-02-23 07:13:41 +0100

    Final cleanup and documentation update

--- test-env.js
+++
@@ -1,9 +0,0 @@
-console.log('--- Environment Check ---');
-console.log('GITHUB_REPOSITORY:', process.env.GITHUB_REPOSITORY);
-console.log('PORT:', process.env.PORT);
-console.log('GH_TOKEN PRESENT:', !!process.env.GH_TOKEN);
-if (process.env.GH_TOKEN) {
-    console.log('GH_TOKEN LENGTH:', process.env.GH_TOKEN.length);
-}
-console.log('---------------------------');
-process.exit(0);
